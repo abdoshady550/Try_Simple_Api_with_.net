7f3d92b130871fdc5f2e748c24e984f3159f96bd
Abdelrahman <abdoshady550@gmail.com>
2025-06-03 14:27:07 +0300

    Update rate limiting middleware by IP address tracking
    Implemented `RateLimiterMiddleware` to limit requests
    to 5 per 10 seconds per IP address using `IMemoryCache`.
    Added `AddMemoryCache` to the service collection and
    corrected the middleware registration name in the
    application pipeline.

---
+++ Meddlewares/RateLimiterMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace Asp.net_Web_Api.Meddlewares
+{
+
+
+    public class RateLimiterMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IMemoryCache _memoryCache;
+
+        public RateLimiterMiddleware(RequestDelegate next, IMemoryCache memoryCache)
+        {
+            _next = next;
+            _memoryCache = memoryCache;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var ip = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+            var cacheKey = $"RateLimit_{ip}";
+
+            var requestInfo = _memoryCache.GetOrCreate(cacheKey, entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10);
+                return new RequestCounter();
+            });
+
+            requestInfo!.Count++;
+
+
+            if (requestInfo.Count > 5)
+            {
+                context.Response.StatusCode = 429;
+                await context.Response
+                    .WriteAsync($"Rate limit exceeded, Please wait and try again later.");
+                return;
+
+            }
+
+            await _next(context);
+        }
+
+        private class RequestCounter
+        {
+            public int Count { get; set; } = 0;
+        }
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -11,12 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
+
+
 var configuration = new ConfigurationBuilder()
      .AddJsonFile("appsettings.json")
      .Build();
 var connStr = configuration.GetSection("constr").Value;
-
-
 
 builder.Services.AddDbContext<AppDbContext>(option => option
 .UseSqlServer(connStr));
@@ -29,7 +30,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseMiddleware<RateLimeterMeddleware>();
+app.UseMiddleware<RateLimiterMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
